4d6c60c5547f9af4dda753b9fa04d70639292beb
Rick Kock <rit.kock@gmail.com>
2023-06-01 03:41:26 +0200

    Replace hardcoded cloning logic with array loop

--- microservice-setup.sh
+++ microservice-setup.sh
@@ -8,13 +8,27 @@
 # Clones all git repositories that are part of this microservice architecture.
 
 clone_repos() {
-    git clone https://github.com/RickITKock/microservice-posts.git
-    git clone git@github.com:RickITKock/microservice-client.git
-    git clone git@github.com:RickITKock/microservice-infra.git
-    git clone git@github.com:RickITKock/microservice-comments.git
-    git clone git@github.com:RickITKock/microservice-event-bus.git
-    git clone git@github.com:RickITKock/microservice-moderation.git
-    git clone git@github.com:RickITKock/microservice-query.git
+    prefix=microservice
+    repos=(
+        posts 
+        client 
+        infra 
+        comments 
+        event-bus 
+        moderation 
+        query)
+
+    for repo in "${repos[@]}"; do
+        dir_name="$prefix-$repo"
+        if [ -d "$dir_name" ]; then
+            echo "$dir_name already exists"
+        else
+            git clone "git@github.com:RickITKock/$dir_name.git"
+        fi
+        printf '\n'
+    done
+
+    # echo ${repos[@]}
 }
 
 echo 'Run this script to clone all git repositories.'
@@ -22,10 +36,10 @@ echo 'Do you wish to clone all microservice related projects? [y/N]'
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    echo 'Cloning all repositories...'
+    echo '\nCloning all repositories...'
     clone_repos
 else
-    echo 'Exiting...'
+    echo '\nExiting...'
 fi
 
 
